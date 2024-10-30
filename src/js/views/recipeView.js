7db4cdf4db7ffb36182ec04834eccf31e5d85d0b
Insha Mahfooz <insha8471@gmail.com>
2024-10-31 00:48:53 +0530

    fraction error resolve

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './View.js';
 
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+
 
 class RecipeView extends View {
     _parentElement = document.querySelector('.recipe');
@@ -125,7 +125,7 @@ class RecipeView extends View {
                 <svg class="recipe__icon">
                     <use href="${icons}#icon-check"></use>
                 </svg>
-                <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+                <div class="recipe__quantity">${ing.quantity ? Math.ceil(ing.quantity) : ''}</div>
                 <div class="recipe__description">
                     <span class="recipe__unit">${ing.unit}</span>
                     ${ing.description}  
